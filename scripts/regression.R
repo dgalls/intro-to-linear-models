b876eacf8eaaa535c0eab1453708dcad54d0613f
dgalls <d.gallagher@uea.ac.uk>
2023-02-24 13:50:37 +0000

    up to 15.8

--- scripts/regression.R
+++ scripts/regression.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(rstatix)
 library(performance)
+library(patchwork)
 
 janka <- read_csv(here("data", "janka.csv"))
 
@@ -48,7 +49,64 @@ janka %>%
 
 # CONFIDENCE LEVELS ----
 
-broom::tidy(janka_ls1, conf.int=T, conf.level=0.95)
+#broom::tidy(janka_ls1, conf.int=T, conf.level=0.95)     # tidyverse
+
+confint(janka_ls1)                                      # base R
+
+# EFFECT SIZE ----
+
+janka_ls1 %>% 
+  broom::glance()
+
+# CHECKING ASSUMPTIONS ----
+
+# checking that the unexplained variation around the regression line (residuals)
+# is (approx) normally distributed and has constant variance
+
+janka_ls1 %>% 
+  broom::augment() %>% 
+  head()
+
+augmented_ls1 <- janka_ls1 %>% 
+  broom::augment()
+
+augmented_ls1 %>% 
+  ggplot(aes(x=dens, 
+             y=.fitted))+
+  geom_line()+ 
+  geom_point(aes(x=dens, 
+                 y=hardness))+
+  geom_segment(aes(x=dens, 
+                   xend=dens, 
+                   y=.fitted, 
+                   yend=hardness), 
+               linetype="dashed", colour="red")
+
+# checking that (1) residuals are normally distributed (2) residuals are homogenous
+
+model_plot <- function(data=augmented_ls1, 
+                       x="dens", 
+                       y="hardness", 
+                       title="Full data"){
+  ggplot(aes(x=.data[[x]], 
+             y=.data[[y]]), 
+         data=data)+
+    geom_line()+
+    theme_bw()+
+    ggtitle(title)
+}
+
+p1 <- model_plot() # A line connecting all the data points in order 
+p2 <- model_plot(y=".fitted", title="Linear prediction") # Plotting the fitted values against the independent e.g. our regression line
+p3 <- model_plot(y=".resid", title="Remaining pattern") # Plotting the residuals against the fitted values e.g. remaining variance
+
+
+
+
+
+
+
+
 
 
 
